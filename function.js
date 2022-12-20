ee0d20cecc8696d7b2f535c37b55d264e88d3508
f3rnandovil <ingfernandovv@gmail.com>
2022-12-19 18:11:24 -0600

    First commit

---
+++ function.js
@@ -0,0 +1,15 @@
+const but = document.querySelector('button.button-15');
+
+but.addEventListener("mouseover", mouseHover)
+
+function f1() {
+    alert("Ahueeevooo");
+// let num = prompt("Cual es tu numero de telefono")
+// console.log(num)
+}
+function mouseHover() {
+    const i = Math.floor(Math.random()*500)+1 ;
+    const j = Math.floor(Math.random()*500)+1 ;
+    but.style.left=i+"px";
+    but.style.top=j+"px";
+}
